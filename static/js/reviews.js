4f1a0ba31e7e5e20d4b6ec12438b226143a130fc
Aled Jones <apps@aledjones.net>
2017-05-07 01:38:46 +0100

    style tweaks and transition

--- static/js/reviews.js
+++ static/js/reviews.js
@@ -79,39 +79,51 @@
     }
 
     function draw(id) {
-    var build = new google.visualization.DataTable();
-    build.addColumn('string', 'Time');
-    if (id == 'num') {
-        build.addColumn('number', 'Number of Tweets');
-    }
-    if (id == 'summed') {
-        build.addColumn('number', 'Total Sentiment');
-    }
-    build.addRows(buildData(['dt', id]));
-
-    var options = {
-        // colors: config.colors,
-        // hAxis: {
-        //     slantedText: true,
-        //     showTextEvery: getTickInterval()
-        // },
-        chartArea: {
-            top: 30,
-            bottom: 80,
-            left: 70,
-            right: 40,
-            width: '100%',
-            height: '100%'
-        },
-        theme: {
-            legend: {
-                position: 'none'
-            }
+        var build = new google.visualization.DataTable();
+        build.addColumn('string', 'Time');
+        if (id == 'num') {
+            build.addColumn('number', 'Number of Tweets');
         }
-    };
+        if (id == 'summed') {
+            build.addColumn('number', 'Total Sentiment');
+        }
+        build.addRows(buildData(['dt', id]));
 
-    var chart = new google.visualization.LineChart(document.getElementById(id));
-    chart.draw(build, options);
-};
+        var options = {
+            colors: id == 'summed' ? ['rgb(220,100,50)'] : null,
+            hAxis: {
+                slantedText: true,
+                // showTextEvery: getTickInterval()
+                textStyle: { color: 'rgb(150,150,150)' }
+            },
+            vAxis: {
+                textStyle: { color: 'rgb(150,150,150)' },
+                gridlines: {
+                    color: "rgb(100,100,100)"
+                },
+                baselineColor: 'rgb(150,150,150)'
+            },
+            chartArea: {
+                top: 30,
+                bottom: 80,
+                left: 70,
+                right: 40,
+                width: '100%',
+                height: '100%',
+                backgroundColor: {
+                    fill: 'rgb(55,55,55)'
+                }
+            },
+            backgroundColor: 'transparent',
+            theme: {
+                legend: {
+                    position: 'none'
+                }
+            }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById(id));
+        chart.draw(build, options);
+    };
 
 })();
